0726480979e881a04997ec9b1a5ef7fbcc0909ac
taxigps <taxigps@sina.com>
2015-08-24 14:13:48 +0800

    [service.subtitles.subhd] update to v1.0.5

--- service.subtitles.subhd/service.py
+++ service.subtitles.subhd/service.py
@@ -6,7 +6,6 @@ import sys
 import xbmc
 import urllib
 import urllib2
-import shutil
 import xbmcvfs
 import xbmcaddon
 import xbmcgui,xbmcplugin
@@ -111,8 +110,18 @@ def Search( item ):
                                                                         )
             xbmcplugin.addDirectoryItem(handle=int(sys.argv[1]),url=url,listitem=listitem,isFolder=False)
 
+def rmtree(path):
+    if isinstance(path, unicode):
+        path = path.encode('utf-8')
+    dirs, files = xbmcvfs.listdir(path)
+    for dir in dirs:
+        rmtree(os.path.join(path, dir))
+    for file in files:
+        xbmcvfs.delete(os.path.join(path, file))
+    xbmcvfs.rmdir(path) 
+
 def Download(url,lang):
-    try: shutil.rmtree(__temp__)
+    try: rmtree(__temp__)
     except: pass
     try: os.makedirs(__temp__)
     except: pass
