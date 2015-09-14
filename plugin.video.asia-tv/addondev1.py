4596266684582e19927bfd484cbd4e18acb5d75f
米老康 <sun_kang@hotmail.com>
2015-09-14 16:41:56 -0400

    Version 1.0.3
    2.0.1
    
    2.0.1
    
    wrong
    
    1.0.3
    
    1.0.4
    
    remove resources
    
    update tv_listing.txt
    
    plugin.video.asia-tv-2.0.1.zip
    
    [plugin.video.cntv-replay] update to 1.0.4
    
    [plugin.video.asia-tv] Update to 2.0.2
    
    [plugin.video.asia-tv] Update to 2.0.3
    
    [plugin.video.asia-tv] Update to 2.0.3

--- plugin.video.asia-tv/addondev1.py
+++ plugin.video.asia-tv/addondev1.py
@@ -1,53 +1,50 @@
-import xbmcplugin
-import xbmcgui
-import xbmcaddon
-import xbmc
-import sys
-import dircache
-import fnmatch
-import operator
 import os
+import sys
+import urllib
+import urlparse
+import xbmcgui
+import xbmcplugin
+import xbmcaddon
 
+base_url = sys.argv[0]
+addon_handle = int(sys.argv[1])
+args = urlparse.parse_qs(sys.argv[2][1:])
 addon = xbmcaddon.Addon()
-thisPlugin = int(sys.argv[1])
 pwd_path=addon.getAddonInfo('path')
-f = open(os.path.join(pwd_path,'channel_list.txt'),'r')
-tv_dict= {}
-for line in f:
-    if line.startswith('channel'): 
-        key, value =line.split()
-        tv_dict[key] = value
-        xbmc.log(  key + ' =' + tv_dict[key])
-f.close
-xbmc.log('#=#=#=# fufill the dictionary #=#=#`=#')
 
-def createListing():
-    dirContent = dircache.listdir(pwd_path)
-    #parse contents for all .flv files
-    dirContent = fnmatch.filter(dirContent, '*.flv')
-    #create listing
-    listing = []
-    for file in dirContent:
-        uri = 'http://fms.cntv.lxdns.com/live/flv/' + file
-        label = file.replace('.flv','')
-        label = label.replace('channel','')
-        
-        # 
-        if file in tv_dict: 
-            xbmc.log( '#=#=#=# ' +  file + '=' + tv_dict[file] + ' #=#=#=#')
-            label = tv_dict[file] + ' ' + label.zfill(3)
-        else:
-            label = label.zfill(3)
-        listing.append([label,uri])
-    listing=sorted(listing, key=operator.itemgetter(0))
-    return listing
+xbmcplugin.setContent(addon_handle, 'movies')
 
-def sendToXbmc(listing):
-    global thisPlugin
-    for item in listing:
-    	listItem = xbmcgui.ListItem(item[0])
-    	xbmcplugin.addDirectoryItem(thisPlugin,item[1],listItem)
+def build_url(query):
+    return base_url + '?' + urllib.urlencode(query)
 
-    xbmcplugin.endOfDirectory(thisPlugin)
+mode = args.get('mode', None)
 
-sendToXbmc(createListing())
+if mode is None:
+    f = open(os.path.join(pwd_path,'cat_list.txt'),'r')
+    for i in f:
+        if i.startswith('Y'): 
+            Y,cat,sym =i.split()
+            url = build_url({'mode': 'folder', 'foldername': cat, 'symbol': sym })
+            li = xbmcgui.ListItem(cat, iconImage=pwd_path+'/'+'icon.png')
+            xbmcplugin.addDirectoryItem(handle=addon_handle, url=url,
+                                listitem=li, isFolder=True)
+    f.close()
+    xbmcplugin.endOfDirectory(addon_handle)
+
+elif mode[0] == 'folder':
+    foldername = args['foldername'][0]
+    symbol=args['symbol'][0]
+    print 'cat='+foldername+' symbol='+symbol
+    f = open(os.path.join(pwd_path,'tv_list.txt'),'r')
+    tv_listing = []                                          
+    for i in f:
+        if i and i[0].isalpha(): 
+            id,label,url =i.split()
+            if symbol in id and 'N'not in id:
+                tv_listing.append([label,url])
+    f.close()
+    tv_listing.sort()
+    for i in tv_listing:
+        li = xbmcgui.ListItem(i[0], iconImage='icon.png')
+        xbmcplugin.addDirectoryItem(handle=addon_handle, url=i[1], listitem=li)
+    xbmcplugin.endOfDirectory(addon_handle)
