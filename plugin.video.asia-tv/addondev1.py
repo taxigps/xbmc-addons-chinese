a1c502e37cfc677a4a2bf539cd223d08b52bdf4c
米老康 <sun_kang@hotmail.com>
2015-09-21 22:42:05 -0400

    [plugin.video.asia-tv] Update to 2.0.3

--- plugin.video.asia-tv/addondev1.py
+++ plugin.video.asia-tv/addondev1.py
@@ -6,6 +6,7 @@ import xbmcgui
 import xbmcplugin
 import xbmcaddon
 
+
 base_url = sys.argv[0]
 addon_handle = int(sys.argv[1])
 args = urlparse.parse_qs(sys.argv[2][1:])
@@ -25,7 +26,7 @@ if mode is None:
         if i.startswith('Y'): 
             Y,cat,sym =i.split()
             url = build_url({'mode': 'folder', 'foldername': cat, 'symbol': sym })
-            li = xbmcgui.ListItem(cat, iconImage='icon.png')
+            li = xbmcgui.ListItem(cat, iconImage=pwd_path+'/'+'icon.png')
             xbmcplugin.addDirectoryItem(handle=addon_handle, url=url,
                                 listitem=li, isFolder=True)
     f.close()
@@ -36,11 +37,15 @@ elif mode[0] == 'folder':
     symbol=args['symbol'][0]
     print 'cat='+foldername+' symbol='+symbol
     f = open(os.path.join(pwd_path,'tv_list.txt'),'r')
+    tv_listing = []                                          
     for i in f:
         if i and i[0].isalpha(): 
             id,label,url =i.split()
             if symbol in id and 'N'not in id:
-                li = xbmcgui.ListItem(label, iconImage='icon.png')
-                xbmcplugin.addDirectoryItem(handle=addon_handle, url=url, listitem=li)
-    xbmcplugin.endOfDirectory(addon_handle)
+                tv_listing.append([label,url])
     f.close()
+    tv_listing.sort()
+    for i in tv_listing:
+        li = xbmcgui.ListItem(i[0], iconImage='icon.png')
+        xbmcplugin.addDirectoryItem(handle=addon_handle, url=i[1], listitem=li)
+    xbmcplugin.endOfDirectory(addon_handle)
