5d57d0596babb5dac7eb952c9ac4d7e01e4b67eb
taxigps <taxigps@sina.com>
2016-03-01 14:01:45 +0800

    Squashed 'plugin.video.rrmj/' changes from 7824f9e..2b8680a
    2b8680a fix addon.xml
    e9227fe Update README.md
    49d081b bump version to 0.1.3
    22638a2 clean up
    6dcbbd4 add auto play setting
    5b80fe3 Update README.md
    b08372d update readme
    67caef2 bump version to 0.1.2
    5a23704 provides audio plugin to enable auto play next
    4a16442 Update README.md
    0c8fffc typo
    eb82515 bump version to 0.1.1
    93d672e better history
    
    git-subtree-dir: plugin.video.rrmj
    git-subtree-split: 2b8680a24d208bffb17f146cbc1b93e7cf23c375

--- README.md
+++ README.md
@@ -2,6 +2,8 @@
 
 ## todo list
 - [x] auto play next episode
+- ~~设置-->视频-->播放-->自动播放下一个视频~~
+- ~~Settings-->Video-->Playback-->Play next video automatically~~
 - [ ] download and store
 - [x] search
-- [ ] history
+- [x] history
--- resources/lib/common.py
+++ resources/lib/common.py
@@ -5,11 +5,24 @@ import urllib2
 import gzip
 import StringIO
 import re
+import json
 import traceback
 
 
 def colorize(label, color):
     return "[COLOR %s]" % color + label + "[/COLOR]"
+
+
+def setSettingByRPC(key, value):
+    result = xbmc.executeJSONRPC('{"jsonrpc":"2.0", "method":"Settings.SetSettingValue", "params":{"setting":"%s", "value":%s}, "id":1}' % (key, value))
+    result = json.loads(result)
+    return result
+
+
+def getSettingByRPC(key):
+    result = xbmc.executeJSONRPC('{"jsonrpc":"2.0","method":"Settings.GetSettingValue","params":{"setting":"%s"},"id":1}' % key)
+    result = json.loads(result)
+    return result["result"]["value"]
 
 
 def GetHttpData(url, data=None, cookie=None, headers=None):
