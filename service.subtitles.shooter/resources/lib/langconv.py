4f872172fc84352ecb6d11d2dbc1a25c3c2007cd
taxigps <taxigps@sina.com>
2020-04-19 18:38:36 +0800

    [service.subtitles.shooter] 2.0.0 v2

--- service.subtitles.shooter/resources/lib/langconv.py
+++ service.subtitles.shooter/resources/lib/langconv.py
@@ -10,24 +10,9 @@ try:
 except:
     pass
 
-try:
-    from zh_wiki import zh2Hant, zh2Hans
-except ImportError:
-    from zhtools.zh_wiki import zh2Hant, zh2Hans
+from zh_wiki import zh2Hant, zh2Hans
 
-import sys
-py3k = sys.version_info >= (3, 0, 0)
-
-if py3k:
-    UEMPTY = ''
-else:
-    _zh2Hant, _zh2Hans = {}, {}
-    for old, new in ((zh2Hant, _zh2Hant), (zh2Hans, _zh2Hans)):
-        for k, v in old.items():
-            new[k.decode('utf8')] = v.decode('utf8')
-    zh2Hant = _zh2Hant
-    zh2Hans = _zh2Hans
-    UEMPTY = ''.decode('utf8')
+UEMPTY = ''
 
 # states
 (START, END, FAIL, WAIT_TAIL) = list(range(4))
